daf420ccb469044e91aaf759d96a7b092edbbdc0
Nadia Amouei <nadiaamouei09@gmail.com>
2025-01-17 16:37:19 +0100

    auth index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,30 +5,15 @@ import express from "express";
 import { router } from "./router";
 import cors from "cors";
 import { db } from "./models/index";
-import session from "express-session";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const SECRET = process.env.SECRET || "ANOTHER_SECRET";
 
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use(
-  session({
-    name: "sid",
-    saveUninitialized: false,
-    resave: false,
-    secret: SECRET,
-    cookie: {
-      maxAge: 1000 * 60 * 60,
-      sameSite: true,
-      httpOnly: false,
-      secure: false,
-    },
-  })
-);
+
 app.use(router);
 
 (async () => {
