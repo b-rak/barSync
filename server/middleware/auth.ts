d97706c1799f0b7fe45342a0bb227353675117d1
Nadia Amouei <nadiaamouei09@gmail.com>
2025-01-17 16:31:31 +0100

    auth refactored to jwt

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,16 +1,26 @@
 import { db } from "../models";
 import { Request, Response, NextFunction } from "express";
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.SECRET_KEY || 'default';
 
 const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { id } = req.session;
-    const user = await db.user.findOne({where: { id: id }});
-    if (!user) throw new Error();
-    req.user = user;
-    next();
-  } catch (error) {
-    return res.sendStatus(401);
+    const authHeaders = req.headers['authorization'];
+    if (!authHeaders) return res.sendStatus(403);
+    const token = authHeaders.split(' ')[1];
+
+    try {
+      const { id } = jwt.verify(token, SECRET_KEY);
+      
+      const user = await db.user.findOne({where: { id: id }});
+      if (!user) return res.sendStatus(401);
+      req.user = user;
+  
+      next();
+    } catch (error) {
+      res.sendStatus(401);
+    }
   }
-};
+}
 
 export default authMiddleware;
